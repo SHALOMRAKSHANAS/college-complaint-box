9544cb545bf7a9416083b934d457c4ba6db47700
SHALOMRAKSHANAS <tstr2133.shalomrakjshanas@gmail.com>
2025-07-07 15:35:55 +0530

    Update ComplaintForm.jsx

--- src/pages/ComplaintForm.jsx
+++ src/pages/ComplaintForm.jsx
@@ -36,7 +36,7 @@ function ComplaintForm() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/complaints', {
+      const response = await fetch('https://chat.lokihere.me/complaints', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text, category })
